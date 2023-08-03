a7531fc73f971ca9a87b4b215ff58129a4947e8a
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-08-03 10:28:57 +0530

    Added comments

--- TF_Basics_1.py
+++ TF_Basics_1.py
@@ -6,23 +6,28 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 print(tf.__version__)
 
+# Tensor
 tensor_a = tf.constant(8)
 print(tensor_a)
 
 tensor_a = tf.constant(8.0)
 print(tensor_a)
 
+# Shape
 tensor_a = tf.constant([[10, 20, 30], [40, 50, 60]], shape=(3, 2))
 print(tensor_a)
 
+# dtype
 tensor_a = tf.constant(8, shape=(1, 1), dtype=tf.float32)
 print(tensor_a)
 
+# Matrix
 tensor_a = tf.ones((4, 4))
 print(tensor_a)
 
 tensor_a = tf.zeros((4, 4))
 print(tensor_a)
 
+# Identity Matrix
 tensor_a = tf.eye(5, dtype=tf.int32)
 print(tensor_a)
